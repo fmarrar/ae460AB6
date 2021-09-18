642e1c23fd0ff2a737c1367bf153ab1a2b44f4a1
alanh <alanh@LAPTOP-Q5SRV0PD>
2021-09-17 23:24:44 -0500

    Normal, axial, and moment coefficients

--- Lab_2/ae460_lab2.m
+++ Lab_2/ae460_lab2.m
@@ -15,9 +15,16 @@ param.Properties.VariableNames = varNames; % apply variable names
 opts = detectImportOptions(filename, 'Range', 'J4'); % x/c, y/c, pressures
 dist = readtable(filename, opts);
 % extract variables as arrays
-x = dist{1,1:end-1}; % x/c
-y = dist{2,1:end-1}; % y/c
-DeltaP = dist{3:end,1:end-1}; % differential pressures
+x = dist{1,:}; % x/c
+y = dist{2,:}; % y/c
+DeltaP = dist{3:end,:}; % differential pressures
 
 %%
 C_p = DeltaP ./ param.q_WT_corr;
+C_n = sum(0.5*(C_p(:,1:end-1)+C_p(:,2:end)).*(x(2:end)-x(1:end-1)),2);
+C_a = sum(0.5*(C_p(:,1:end-1)+C_p(:,2:end)).*(y(1:end-1)-y(2:end)),2);
+tmp = C_p.*x;
+Xarm = sum(0.5*(tmp(:,1:end-1)+tmp(:,2:end).*(x(1:end-1)-x(2:end))),2);
+tmp = C_p.*y;
+Yarm = sum(0.5*(tmp(:,1:end-1)+tmp(:,2:end).*(y(1:end-1)-y(2:end))),2);
+C_mLE = Xarm + Yarm;
