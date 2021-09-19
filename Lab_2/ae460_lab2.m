20f09d4693c9d7ace92ba3f3f4b40f4b1de4972c
alanh <alanh@LAPTOP-Q5SRV0PD>
2021-09-18 20:17:50 -0500

    further progress on plotting, organized catch_22

--- Lab_2/ae460_lab2.m
+++ Lab_2/ae460_lab2.m
@@ -1,8 +1,14 @@
-%% Import data
+% ae460_lab2.m ... Code for Lab 2
+
 % clean up
 clear
 close all
 
+set(0,'defaultaxesfontname','times')
+set(0,'defaultaxesfontsize',10)
+set(0,'defaultFigurePosition',[360 198 460 360])
+
+%% Import data
 filename = 'data2.csv'; % file to read from
 % environment
 opts = detectImportOptions(filename, 'Range', 'A:G'); % select parameters
@@ -42,13 +48,76 @@ C_l = C_n.*cosd(param.AOA) - C_a.*sind(param.AOA);
 C_mQC = C_mLE + 0.25*C_l;
 
 %% Plotting
-% C_p vs. x/c
+% C_p vs. x/c (linear region)
 figure(1), clf
-grid on
 hold on
+% find index of maximum AOA
 max_AOA_idx = find(param.AOA == max(param.AOA));
-[indices,void] = find(param.AOA(1:max_AOA_idx) == [-4,0,4,8,12]);
+% grab C_p distributions for increasing AOA
+[indices,~] = find(param.AOA(1:max_AOA_idx) == [-4,0,4,8,12]);
 for idx=indices
-    plot(x,C_p(idx,:))
+    plot(x, C_p(idx,:))
 end
+hold off
 set(gca, 'ydir', 'reverse')
+grid on
+
+xlabel('\itx/c', 'FontSize', 12)
+ylabel('\itC_p', 'FontSize', 12)
+
+set(gca, 'Box', 'off', 'TickDir', 'out', 'TickLength', [.02,.02],...
+    'XMinorTick', 'on', 'YMinorTick','on')
+set(get(gca,'ylabel'), 'rotation', 0,...
+    'VerticalAlignment', 'middle', 'HorizontalAlignment', 'right')
+ax = gca;
+ax.XAxis.LineWidth = 1;
+ax.YAxis.LineWidth = 1;
+
+% C_p vs. x/c (near/at stall)
+figure(2), clf
+hold on
+[indices,~] = find(param.AOA(1:max_AOA_idx) == [8,16,20,22,26]);
+for idx=indices
+    plot(x, C_p(idx,:))
+end
+hold off
+set(gca, 'ydir', 'reverse')
+grid on
+
+xlabel('\itx/c', 'FontSize', 12)
+ylabel('\itC_p', 'FontSize', 12)
+
+set(gca, 'Box', 'off', 'TickDir', 'out', 'TickLength', [.02,.02],...
+    'XMinorTick', 'on', 'YMinorTick', 'on')
+set(get(gca,'ylabel'), 'rotation', 0,...
+    'VerticalAlignment', 'middle', 'HorizontalAlignment', 'right')
+ax = gca;
+ax.XAxis.LineWidth = 1;
+ax.YAxis.LineWidth = 1;
+
+% C_l vs. AOA
+figure(3), clf
+lin_bndy = find(param.AOA(1:max_AOA_idx) == 6); % sample linear region
+b = [ones(lin_bndy,1) param.AOA(1:lin_bndy)] \ C_l(1:lin_bndy);
+b(2) = 2*pi/rad2deg(1); % slope from thin airfoil theory
+TAT = b(2)*param.AOA(1:max_AOA_idx) + b(1);
+hold on
+plot(param.AOA(1:max_AOA_idx), C_l(1:max_AOA_idx),...
+    'LineStyle', 'none', 'Marker', 'o', 'MarkerSize', 6)
+plot(param.AOA(max_AOA_idx+1:end), C_l(max_AOA_idx+1:end),...
+    'LineStyle', 'none', 'Marker', 's', 'MarkerSize', 7)
+plot(param.AOA(1:max_AOA_idx), TAT)
+hold off
+grid on
+v_padding = abs(TAT(1) - C_l(1));
+axis([param.AOA(1), param.AOA(max_AOA_idx), TAT(1), max(C_l)+v_padding])
+xlabel('\alpha (deg)', 'FontSize', 12)
+ylabel('\itC_l', 'FontSize', 12)
+
+set(gca, 'Box', 'off', 'TickDir', 'out', 'TickLength', [.02,.02],...
+    'XMinorTick', 'on', 'YMinorTick', 'on')
+set(get(gca,'ylabel'), 'rotation', 0,...
+    'VerticalAlignment', 'middle', 'HorizontalAlignment', 'right')
+ax = gca;
+ax.XAxis.LineWidth = 1;
+ax.YAxis.LineWidth = 1;
